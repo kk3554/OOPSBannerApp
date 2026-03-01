edc0da1934f390797c934cce14af1b2358e5598f
kk3554 <kk3554@srmist.edu.in>
2026-03-01 22:24:14 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,1 +1,25 @@
 
+public class OOPSArray{
+    public static void main(String[] args) {
+
+        String[] lines = {
+            " ***     ***     *****   ***** ",
+			
+            "*   *   *   *    *    *  *     ",
+			
+            "*   *   *   *    *    *  *     ",
+			
+            "*   *   *   *    *****   ***** ",
+			
+            "*   *   *   *    *           * ",
+			
+            "*   *   *   *    *           * ",
+			
+            " ***     ***     *       ***** "
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
