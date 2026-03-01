dd6a3e61deb68a571418281a19985b842b2d1ebd
kk3554 <kk3554@srmist.edu.in>
2026-03-01 22:00:45 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,1 +1,13 @@
+public class OOPSBanner {
 
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****   ******   ****** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******   ****** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println(" *****    *****   *        ****** ");
+    }
+}
